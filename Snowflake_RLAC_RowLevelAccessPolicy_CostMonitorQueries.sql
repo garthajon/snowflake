f0d08e93fb754ad31f0d81ce4d8293a02aa21cc9
Garth Jones <garthjon@outlook.com>
2025-08-27 06:53:33 +0100

    complex cost query for patient_contact added

--- Snowflake_RLAC_RowLevelAccessPolicy_CostMonitorQueries.sql
+++ Snowflake_RLAC_RowLevelAccessPolicy_CostMonitorQueries.sql
@@ -47,13 +47,29 @@ GROUP BY USE_CONCEPT_ID;
 -- RESET TAGGING TO BLANK
 ALTER SESSION SET QUERY_TAG = '';
 
+-- RESET TAGGING TO BLANK
+ALTER SESSION SET QUERY_TAG = '';
+
 -- A MORE COMPLEX QUERY TO TEST CREDIT USAGE (WITH A JOIN)
 
 -- group by query for table with row level access
 use role db_role_project1;
+use warehouse snowflake_learning_wh;
 ALTER SESSION SET QUERY_TAG = 'PATIENT_CONTACT WITH RLAC';
 SELECT COUNT(*) AS COUNTALL, PATIENT_CONTACT.USE_CONCEPT_ID FROM PATIENT_CONTACT
 LEFT JOIN PATIENT_CONTACT AS PAT2 ON PATIENT_CONTACT.LDS_RECORD_ID = PAT2.LDS_RECORD_ID 
+full outer join  PATIENT_CONTACT AS PAT3 ON PATIENT_CONTACT.LDS_RECORD_ID = PAT3.LDS_RECORD_ID 
+and pat3.use_concept_id = 'HomePhone' and pat3.lds_is_deleted = 'FALSE'
+full outer join  PATIENT_CONTACT AS PAT4 ON PATIENT_CONTACT.LDS_RECORD_ID = PAT4.LDS_RECORD_ID 
+and pat4.use_concept_id = 'HomePhone' and pat4.lds_is_deleted = 'FALSE'
+RIGHT outer join  PATIENT_CONTACT AS PAT5 ON PATIENT_CONTACT.LDS_RECORD_ID = PAT5.LDS_RECORD_ID 
+and pat5.use_concept_id = 'HomePhone' and pat5.lds_is_deleted = 'FALSE'
+INNER JOIN   PATIENT_CONTACT AS PAT6 ON PATIENT_CONTACT.LDS_RECORD_ID = PAT6.LDS_RECORD_ID 
+AND (SUBSTR(PAT6.LDS_BUSINESS_KEY,2,1) = 'A' OR SUBSTR(PAT6.LDS_BUSINESS_KEY,2,1) = 'D'  OR SUBSTR(PAT6.LDS_BUSINESS_KEY,2,1) = 'F')
+inner join 
+(select distinct pat1.lds_record_id as inner_id from PATIENT_CONTACT as pat1,PATIENT_CONTACT as pat2 where SUBSTR(PAT1.LDS_BUSINESS_KEY,2,1) = 'A' 
+and  (SUBSTR(PAT2.LDS_BUSINESS_KEY,3,1) <> 'B' OR SUBSTR(PAT2.LDS_BUSINESS_KEY,3,1) <> 'X' OR SUBSTR(PAT2.LDS_BUSINESS_KEY,3,1) <> 'Y')) as inner_cartesian_final
+on inner_cartesian_final.inner_id = PATIENT_CONTACT.LDS_RECORD_ID 
 GROUP BY PATIENT_CONTACT.USE_CONCEPT_ID;
 -- RESET TAGGING TO BLANK
 ALTER SESSION SET QUERY_TAG = '';
@@ -61,17 +77,31 @@ ALTER SESSION SET QUERY_TAG = '';
 -- group by query for table without row level access
 use role db_role_project1;
 use warehouse snowflake_learning_wh;
-ALTER SESSION SET QUERY_TAG = 'PATIENT_CONTACT WITHOUT RLAC';
+ALTER SESSION SET QUERY_TAG = 'PATIENT_CONTACT2 WITHOUT RLAC';
 SELECT COUNT(*) AS COUNTALL, PATIENT_CONTACT2.USE_CONCEPT_ID FROM PATIENT_CONTACT2
 LEFT JOIN PATIENT_CONTACT2 AS PAT2 ON PATIENT_CONTACT2.LDS_RECORD_ID = PAT2.LDS_RECORD_ID 
+full outer join  PATIENT_CONTACT2 AS PAT3 ON PATIENT_CONTACT2.LDS_RECORD_ID = PAT3.LDS_RECORD_ID 
+and pat3.use_concept_id = 'HomePhone' and pat3.lds_is_deleted = 'FALSE'
+full outer join  PATIENT_CONTACT2 AS PAT4 ON PATIENT_CONTACT2.LDS_RECORD_ID = PAT4.LDS_RECORD_ID 
+and pat4.use_concept_id = 'HomePhone' and pat4.lds_is_deleted = 'FALSE'
+RIGHT outer join  PATIENT_CONTACT2 AS PAT5 ON PATIENT_CONTACT2.LDS_RECORD_ID = PAT5.LDS_RECORD_ID 
+and pat5.use_concept_id = 'HomePhone' and pat5.lds_is_deleted = 'FALSE'
+INNER JOIN   PATIENT_CONTACT2 AS PAT6 ON PATIENT_CONTACT2.LDS_RECORD_ID = PAT6.LDS_RECORD_ID 
+AND (SUBSTR(PAT6.LDS_BUSINESS_KEY,2,1) = 'A' OR SUBSTR(PAT6.LDS_BUSINESS_KEY,2,1) = 'D'  OR SUBSTR(PAT6.LDS_BUSINESS_KEY,2,1) = 'F')
+inner join 
+(select distinct pat1.lds_record_id as inner_id from PATIENT_CONTACT2 as pat1,PATIENT_CONTACT2 as pat2 where SUBSTR(PAT1.LDS_BUSINESS_KEY,2,1) = 'A' 
+and  (SUBSTR(PAT2.LDS_BUSINESS_KEY,3,1) <> 'B' OR SUBSTR(PAT2.LDS_BUSINESS_KEY,3,1) <> 'X' OR SUBSTR(PAT2.LDS_BUSINESS_KEY,3,1) <> 'Y')) as inner_cartesian_final
+on inner_cartesian_final.inner_id = PATIENT_CONTACT2.LDS_RECORD_ID 
 GROUP BY PATIENT_CONTACT2.USE_CONCEPT_ID;
 
 -- RESET TAGGING TO BLANK
 ALTER SESSION SET QUERY_TAG = '';
 
+
 -- Now we can run a query to check the credits used by these queries
 
 use role accountadmin;
+use warehouse snowflake_learning_wh;
 WITH query_costs AS (
 SELECT
 q.execution_status,
@@ -95,6 +125,7 @@ q.start_time >= DATEADD(DAY, -2, CURRENT_DATE) -- Last 2 days
 
 SELECT 
 query_id,
+START_TIME,
 query_text,
 query_tag,
 execution_status,
@@ -105,9 +136,10 @@ FROM
 query_costs
 where query_tag like '%PATIENT_CONTACT%'
 AND QUERY_TEXT LIKE '%COUNT%'
---AND QUERY_TEXT NOT LIKE '%LEFT JOIN%'
+AND QUERY_TEXT  LIKE '%innerq%'
 AND execution_status = 'SUCCESS'
 GROUP BY 
+START_TIME,
 query_id,
 query_text,
 query_tag,
@@ -115,10 +147,12 @@ TIMESTAMPDIFF('milliseconds', start_time, end_time),
 execution_status
 --error_code
 ORDER BY
-QUERY_TAG DESC
+QUERY_TAG DESC,
+START_TIME DESC
 LIMIT 20;
 
 
+SELECT COUNT(*) AS COUNTALL from PATIENT_CONTACT
 
 
 
