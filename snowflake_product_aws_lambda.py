4236f12f5131a022ee2273e56b7bebe633a6b532
Garth Jones <garthjon@outlook.com>
2025-09-08 18:12:45 +0100

    AWS lambda function to multiply (the product) updated with full commentary

---
+++ snowflake_product_aws_lambda.py
@@ -0,0 +1,71 @@
+# Import necessary modules for the function
+import json
+import functools
+
+# The function itself, in which event is the full request sent to the API
+def lambda_handler(event, context):
+
+    # Declare return variables
+    statusCode = 200
+
+    # dataArray stores the resultset of the function which will be
+    # returned by the function. This begins as an empty array and is added
+    # to in the rows loop below
+    dataArray = []
+
+    # json_compatible_string_to_return is the json body returned by the
+    # function, stored as a string. This empty value is replaced by the real
+    # result when ready
+    json_compatible_string_to_return = ''
+
+    # try/except is used for error handling
+    try:
+        # Retrieve the body of the request as a JSON object
+        body = json.loads(event['body'])
+
+        # Retrieve the 'data' key of the request body
+        # When the request comes from Snowflake, we expect data to be a
+        # an array of each row in the Snowflake query resultset.
+        # Each row is its own array, which begins with the row number,
+        # for example [0, 2, 3] would be the 0th row, in which the
+        # variables passed to the function are 2 and 3. In this case,
+        # the function product output would be 6.
+        rows = body['data']
+
+        # Loop through each row
+        for row in rows:
+
+            # Retrieve the row number from the start of the row array
+            rowNumber = row[0]
+
+            # Retrieve the array of numbers to multiply
+            numbersToMultiply = row[1:]
+
+            # try/except is used for error handling
+            try:
+                # Calculate the rowProduct
+                rowProduct = functools.reduce(lambda a,b : a*b, numbersToMultiply)
+            except:
+                rowProduct = "Error"
+
+            # Create a new array entry
+            newArrayEntry = [rowNumber, rowProduct]
+
+            # Add the newArrayEntry to the main dataArray list
+            dataArray.append(newArrayEntry)
+
+        # Put dataArray into a dictionary, then convert it to a string
+        dataArrayToReturn = {'data' : dataArray}
+        json_compatible_string_to_return = json.dumps(dataArrayToReturn)
+
+    except Exception as err:
+        # Statuscode = 400 signifies an error
+        statusCode = 400
+        # Function will return the error
+        json_compatible_string_to_return = json.dumps({"data":str(err)})
+
+    return {
+        'statusCode': statusCode
+        ,   'headers': { 'Content-Type': 'application/json' }
+        ,   'body' : json_compatible_string_to_return
+        }
